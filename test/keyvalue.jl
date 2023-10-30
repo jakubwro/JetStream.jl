38cac27fa5d26bbf82b36285069e0f1aab660a80
Jakub Wronowski <jakubwro@gmail.com>
2023-10-30 13:55:00 +0100

    kv tests

--- test/keyvalue.jl
+++ test/keyvalue.jl
@@ -1,16 +1,12 @@
 @testset "Key value - 100 keys" begin
     connection = NATS.connect()
-
-    JetStream.keyvalue_create("test_kv"; connection)
     kv = JetStream.KeyValue("test_kv"; connection)
-
     @time @sync for i in 1:100
         @async kv["key_$i"] = "value_$i"
     end
 
     other_conn = NATS.connect()
     kv = JetStream.KeyValue("test_kv"; connection = other_conn)
-
     for i in 1:100
         @test kv["key_$i"] == "value_$i"
     end
